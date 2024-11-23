d9f4e7cb86c85843a84486f53aa566d5c3079f15
TheEnderCore <theendercore@gmail.com>
2024-11-23 21:32:05 +0200

    - updated example

--- src/main/kotlin/org/teamvoided/attuned_convergence/init/mod/CompatExmaple.kt
+++ src/main/kotlin/org/teamvoided/attuned_convergence/init/mod/CompatExmaple.kt
@@ -1,15 +1,39 @@
 package org.teamvoided.attuned_convergence.init.mod
 
 interface CompatExample {
-    //
-    // WOOD COMPAT
-    //
     /*
-    - Leaf Piles
-    - Wood Stairs, Slabs, Walls
-    - Log Piles
-    - Twigs Table
-    - Railings
-    - Boards & there Stairs, Slabs, Walls
-     */
+        // --- WOOD COMPAT ---
+        - Leaf Piles
+        - Wood Stairs, Slabs, Walls
+        - Log Piles
+        - Twigs Table
+        - Railings
+        - Boards & there Stairs, Slabs, Walls
+
+        // --- BIG COMPAT ---
+        - Big Lantern
+        - Lamp (Twigs Big Lamp)
+        - Big Chain
+
+        // --- STONE COMPAT ---
+        - Cracked Stairs, Slabs, Walls
+        - Mossy -> Overgrown
+        - Pillar
+        - Big and Small Grave
+        - Column
+        - Bricks -> Heavy Bricks
+
+        // --- FIRE COMPAT ---
+        (if missing)
+        - Torch
+        - Campfire
+        ---
+        - Candles
+        - Big Candles
+        - Candelabras
+
+        // --- MICRO-BLOCK COMPAT ---
+        - Nubs
+
+    */
 }
