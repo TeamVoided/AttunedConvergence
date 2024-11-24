3df7e58ae6e2c87f11d37b570ab1b9802b9f40af
TheEnderCore <theendercore@gmail.com>
2024-11-24 15:13:11 +0200

    - added TwigsTable to MIG and AP

--- src/main/kotlin/org/teamvoided/attuned_convergence/compat/mod/ArchitectsPaletteCompat.kt
+++ src/main/kotlin/org/teamvoided/attuned_convergence/compat/mod/ArchitectsPaletteCompat.kt
@@ -1,18 +1,29 @@
 package org.teamvoided.attuned_convergence.compat.mod
 
 import architectspalette.core.registry.APBlocks
+import architectspalette.core.registry.util.StoneBlockSet.SetComponent.SLAB
 import org.teamvoided.attuned_convergence.compat.CompatVariables
 import org.teamvoided.attuned_convergence.compat.CompatVariables.ARCHITECTS_PALETTE
+import org.teamvoided.attuned_convergence.compat.module.DnDLeaves
 import org.teamvoided.attuned_convergence.compat.module.DnDLogs
 import org.teamvoided.attuned_convergence.compat.module.DnDWood
 import org.teamvoided.attuned_convergence.compat.module.Module
+import org.teamvoided.attuned_convergence.compat.module.TwigsTable
 
 object ArchitectsPaletteCompat : Compat {
     override fun populate(content: MutableList<Module>) {
         if (CompatVariables.DusksAndDungeons) {
             content.add(DnDWood(ARCHITECTS_PALETTE, "twisted", APBlocks.TWISTED_WOOD.get(), APBlocks.TWISTED_LOG.get()))
-//            content.add(DnDLeaves(ARCHITECTS_PALETTE, "twisted", APBlocks.TWISTED_LEAVES.get()))
+            content.add(DnDLeaves(ARCHITECTS_PALETTE, "twisted", APBlocks.TWISTED_LEAVES.get()))
             content.add(DnDLogs(ARCHITECTS_PALETTE, "twisted", APBlocks.TWISTED_LOG.get()))
+        }
+        if (CompatVariables.Twigs) {
+            content.add(
+                TwigsTable(
+                    ARCHITECTS_PALETTE, "twisted",
+                    APBlocks.TWISTED_PLANKS.get(), APBlocks.TWISTED_PLANKS.getPart(SLAB), APBlocks.TWISTED_FENCE.get()
+                )
+            )
         }
     }
 }
--- src/main/kotlin/org/teamvoided/attuned_convergence/compat/mod/MakeItGothCompat.kt
+++ src/main/kotlin/org/teamvoided/attuned_convergence/compat/mod/MakeItGothCompat.kt
@@ -6,15 +6,22 @@ import org.teamvoided.attuned_convergence.compat.module.ArchitectsPalettePlanks
 import org.teamvoided.attuned_convergence.compat.module.DnDLogs
 import org.teamvoided.attuned_convergence.compat.module.DnDWood
 import org.teamvoided.attuned_convergence.compat.module.Module
+import org.teamvoided.attuned_convergence.compat.module.TwigsTable
 import net.leah.makeitgoth.block.ModBlocks as GothBlocks
 
 object MakeItGothCompat : Compat {
     override fun populate(content: MutableList<Module>) {
         if (CompatVariables.ArchitectsPalette)
             content.add(ArchitectsPalettePlanks(MAKE_IT_GOTH, "rotwood", GothBlocks.ROTWOOD_PLANKS))
-        if (CompatVariables.DusksAndDungeons){
+        if (CompatVariables.DusksAndDungeons) {
             content.add(DnDWood(MAKE_IT_GOTH, "rotwood", GothBlocks.ROTWOOD_WOOD, GothBlocks.ROTWOOD_LOG))
             content.add(DnDLogs(MAKE_IT_GOTH, "rotwood", GothBlocks.ROTWOOD_LOG))
         }
+        if (CompatVariables.Twigs) content.add(
+            TwigsTable(
+                MAKE_IT_GOTH, "rotwood",
+                GothBlocks.ROTWOOD_PLANKS, GothBlocks.ROTWOOD_SLAB, GothBlocks.ROTWOOD_FENCE
+            )
+        )
     }
 }
